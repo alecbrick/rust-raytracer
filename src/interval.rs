e72fae0257eae146ac982f67860706af797fb5e5
Alec Brickner <alecjbrick@gmail.com>
2024-10-28 20:39:44 -0700

    forgot to add interval

---
+++ src/interval.rs
@@ -0,0 +1,43 @@
+use once_cell::sync::Lazy;
+
+pub struct Interval {
+    pub min: f32,
+    pub max: f32,
+}
+
+impl Interval {
+    pub fn new(min: f32, max: f32) -> Interval {
+        Interval {
+            min: min,
+            max: max,
+        }
+    }
+
+    pub fn size(&self) -> f32 {
+        self.max - self.min
+    }
+
+    pub fn contains(&self, x: f32) -> bool {
+        (self.min <= x) && (x <= self.max)
+    }
+
+    pub fn surrounds(&self, x: f32) -> bool {
+        (self.min < x) && (x < self.max)
+    }
+}
+
+impl Default for Interval {
+    fn default() -> Interval {
+        Interval {
+            min: f32::MAX,
+            max: f32::MIN,
+        }
+    }
+}
+
+pub const EMPTY: Lazy<Interval> = Lazy::new(|| {
+    Interval::new(f32::MAX, f32::MIN)
+});
+pub const UNIVERSE: Lazy<Interval> = Lazy::new(|| {
+    Interval::new(f32::MIN, f32::MAX)
+});
